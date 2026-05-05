ab0fced488b2368218e72b0310ff87e416a009cc
Tetsu26xX <gladysra6@gmail.com>
2026-05-05 18:29:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ footer {visibility: hidden;}
 
 /* AJUSTE LOGIN - suave: un poquito más grande y más arriba */
 .login-wrapper {
-    transform: scale(0.95);
+    transform: scale(0.90);
     transform-origin: top center;
-    margin-top: -14px;
+    margin-top: -12px;
 }
 
 </style>
